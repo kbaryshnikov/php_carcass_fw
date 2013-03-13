eb0583e4c6e5268da4fd0b9ca07373e79a11796c
Konstantin Baryshnikov <konstantin@symbi.org>
2013-03-13 07:46:35 +0000

    Unit test: Application_Web_JsonRpcFrontController

---
+++ tests/Unit/Application_Web_JsonRpcFrontControllerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use \Carcass\Application;
+use \Carcass\Corelib;
+use \Carcass\Http;
+
+class Application_Web_JsonRpcFrontControllerTest extends PHPUnit_Framework_TestCase {
+
+    public function testJsonRpcResultsAreDisplayedToResponse() {
+        $Request = $this->getMock('\Carcass\Corelib\Request');
+        $Response = $this->getMockBuilder('\Carcass\Application\Web_Response')->disableOriginalConstructor()->getMock();
+        $Router = $this->getMockBuilder('\Carcass\Application\Web_Router_JsonRpc')->disableOriginalConstructor()->getMock();
+        $Config = $this->getMock('\Carcass\Config\ItemInterface');
+
+        $Server = $this->getMockBuilder('\Carcass\Http\JsonRpc_Server')->disableOriginalConstructor()->getMock();
+        $Server->expects($this->once())
+            ->method('displayTo')
+            ->with(
+                $this->identicalTo($Response)
+            );
+
+        $Ctrl = new TestJsonRpcFrontController($Request, $Response, $Router, $Config);
+        $Ctrl->dispatchJsonRpc($Server, 'TestJsonRpcController.Test', new Corelib\Hash);
+    }
+
+}
+
+class TestJsonRpcFrontController extends Application\Web_JsonRpcFrontController {
+
+    protected function requirePageClass($page_class) {
+        return $page_class;
+    }
+
+}
+
+class TestJsonRpcControllerPage extends Application\Web_PageController {
+
+    public function actionTest() {
+        return true;
+    }
+
+}
