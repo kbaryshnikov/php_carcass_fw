40701744613253c4d496405407583789a7b85750
Konstantin Baryshnikov <konstantin@symbi.org>
2014-02-18 11:33:34 +0000

    version bump

--- lib/Carcass/Application/Instance.php
+++ lib/Carcass/Application/Instance.php
@@ -20,7 +20,7 @@ use Carcass\Connection;
  */
 class Instance {
 
-    const API_VERSION = 20140218;
+    const API_VERSION = 20140219;
 
     protected static $env_defaults = [
         'configuration_name' => null,
