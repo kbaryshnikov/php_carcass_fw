99c2a6a547dfa1c69fd07061a9eb552416314c9a
Konstantin Baryshnikov <konstantin@symbi.org>
2015-10-20 13:59:26 +0300

    Rule/IsAbsoluteUrl: option to allow idn urls. Punycode library imported, encodeUrl method added. API version bump

--- lib/Carcass/Rule/IsAbsoluteUrl.php
+++ lib/Carcass/Rule/IsAbsoluteUrl.php
@@ -8,6 +8,8 @@
 
 namespace Carcass\Rule;
 
+use Carcass\Corelib\Punycode;
+
 /**
  * Class IsAbsoluteUrl
  * @package Carcass\Rule
@@ -19,14 +21,38 @@ class IsAbsoluteUrl extends Base {
      */
     protected $ERROR = 'invalid_url';
 
+    protected $allow_utf8_idn = false;
+
+    protected $IdnDecoder = null;
+
+    public function __construct($allow_utf8_idn = false) {
+        $this->allow_utf8_idn = (bool)$allow_utf8_idn;
+    }
+
     /**
      * @param $value
      * @return bool
      */
     public function validate($value) {
-        return null === $value || preg_match(
-            '[^((https?):\/\/)?(([a-z]|xn--)[a-z0-9\-]*\.)*([a-z]|xn--)([a-z0-9\-]+)(\/([a-z0-9_\-\.~+\']|%[0-9a-f]{2})*)*(\?([a-z0-9+_\-\.=\&:;,]|%[0-9a-f])*)?(#.*)?$]i',
+        return (
+            null === $value
+            || $this->matchesRegexp($value)
+            || ($this->allow_utf8_idn && $this->matchesRegexp($this->decodeIdn($value)))
+        );
+    }
+
+    protected function matchesRegexp($value) {
+        return preg_match(
+            '[^((https?):\/\/)?(([a-z]|xn--)[a-z0-9\-]*\.)*([a-z]|xn--)([a-z0-9\-]+)(\/([\w\d_\-\.~+\']|%[0-9a-f]{2})*)*(\?([\w\d+_\-\.=\&:;,]|%[0-9a-f])*)?(#.*)?$]iu',
             $value
         );
     }
+
+    protected function decodeIdn($value) {
+        if (null === $this->IdnDecoder) {
+            $this->IdnDecoder = new Punycode();
+        }
+        return $this->IdnDecoder->encodeUrl($value);
+    }
+
 }
